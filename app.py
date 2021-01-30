db2e629094ac48477ec17db33508701b2ee2f4c0
Falsorr <74251948+Falsorr@users.noreply.github.com>
2021-01-30 09:44:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,28 +13,42 @@ app = praw.Reddit(client_id = 'FUzb37XqgvY_Kw', client_secret = 'mRsuht9Cb_LPJSQ
                   user_agent = 'McHacks8RedditProject', username = 'IPissedMyBed404', password = 'HePissedTheBed404')
 
 wsb = app.subreddit('Wallstreetbets')
-hot_topics = wsb.hot(limit = 10)
-for submission in hot_topics :
-    print(submission.selftext, '\n\n\n')
     
-    
-
 
 #Creating the UI
 root = tk.Tk()
 canvas = tk.Canvas(root, height = HEIGHT, width = WIDTH)
 canvas.pack()
 
-#The frame1 is used to get the user input, it has a label with instructions, an Entry for the input itself and a button to start searching
-frame1 = tk.Frame(canvas, bg = 'gray')
-frame1.place(relx = 0.1, rely = 0.05, relheight = 0.1, relwidth = 0.8, anchor ='nw')
+#The frame1 is used to get the user input, it has a label with instructions, an Entry for the input itself and buttons to start searching
+frame1 = tk.Frame(canvas, bg = '#CEE3F8')
+frame1.place(relx = 0.05, rely = 0.05, relheight = 0.2, relwidth = 0.9, anchor ='nw')
 
 instruction_fr1 = tk.Label(frame1, text = "Enter the stock code ($??) here: ")
-instruction_fr1.place(rely = 0.1, relx = 0.01, relwidth = 0.4, relheight = 0.8, anchor = 'nw')
+instruction_fr1.place(rely = 0.1, relx = 0.20, relwidth = 0.4, relheight = 0.3, anchor = 'nw')
 
 search_bar = tk.Entry(frame1)
-search_bar.place(rely = 0.1, relx = 0.42, relwidth = 0.2, relheight = 0.8, anchor ='nw')
+search_bar.place(rely = 0.1, relx = 0.60, relwidth = 0.2, relheight = 0.3, anchor ='nw')
 
-search_button = tk.Button(frame1, text = 'Search Reddit!')
-search_button.place(rely = 0.1, relx = 0.67, relwidth = 0.3, relheight = 0.8, anchor = 'nw')
+#Search by the most popular result
+top_button = tk.Button(frame1, text = 'Search by top')
+top_button.place(rely = 0.41, relx = 0.18, relwidth = 0.2, relheight = 0.25, anchor = 'nw')
 
+#Search by the newest result
+new_button = tk.Button(frame1, text = 'Search by new')
+new_button.place(rely = 0.41, relx = 0.40, relwidth = 0.2, relheight = 0.25, anchor = 'nw')
+
+#Search by results with most comments
+hot_button = tk.Button(frame1, text = 'Search by hot')
+hot_button.place(rely = 0.41, relx = 0.62, relwidth = 0.2, relheight = 0.25, anchor = 'nw')
+
+
+
+
+
+#Display frame : The frame where all the info is displayed, it is basically a giant label to write on it
+display_frame = tk.Frame(canvas, bg = '#FF4500')
+display_frame.place(relx = 0.05, rely = 0.3, relwidth = 0.9, relheight = 0.6, anchor = 'nw')
+
+display_label = tk.Label(display_frame, text = "Empty at the moment, start searching Reddit!")
+display_label.place(relx = 0.01, rely = 0.01, relwidth = 0.98, relheight = 0.98, anchor = 'nw')
